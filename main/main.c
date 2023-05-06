033e668d5482f985a2c1fe70ddf69ffc8e1e2dbd
Linh Nguyen <108517804+linhnq172@users.noreply.github.com>
2023-05-07 00:17:06 +0700

    my solution

--- main/main.c
+++ main/main.c
@@ -1,203 +1,190 @@
-/****** Thong tin ********************
-written by Linh Kiện TCP
-Linh Kiện TCP – số 70/ngõ 40, Tạ Quang Bửu, Hai Bà Trưng, Hà Nội.
-Web: Linhkientcp.vn
-Fb : https://www.facebook.com/linhkienTCP/
-Bán hàng: 0966.129.629                 Kỹ thuật: 01663.860.326
-
-***************************/
-
-
 /**** Giới thiệu code ***************
  *  Code sử dụng biến giới hạn là 25 cm để xác định khoảng cách cần dừng.
  *  Khi ta muốn thay đổi khoảng cách, ta thay đổi tham số "gioihan" đang để mặc định là 25. ( đơn vị cm)
- *  Xe ưu tiên rẽ trái trong các trường  hợp phía trước có vật cản. 
- *  Ví dụ: khi khoang cách phía trước <25 , cảm biến đo bên trái trước, nếu không có vật cản thì xe rẽ trái. nếu bên trái cũng có vật cản
- *  lúc đó xe mới kiểm tra bên phải.
- *  Khi cả phía trước, trái ,phải đều có chướng ngại vật. xe đi lùi 1 đoạn, sau đó đo lại khoảng cách. ....
+ *  Xe ban đầu được đặt trạng thái UP, mục tiêu cuối cùng là trạng thái UP
+ *  Khi gặp vật cản, xe tính khoảng cách hai bên trái phải, chọn bên có khoảng cách ngắn hơn, cập nhật trạng thái mới LEFT/RIGHT
+ *  Khi ở trạng thái LEFT/RIGHT, xe sẽ dò phái trước và bên phải/trái, nếu bên phải/trái đi được thì rẽ sang hướng đó, cập nhật lại trạng thái mới
+ *  Nếu không thì tiếp tục đi thẳng cho đến khi tìm được đường rẽ, nếu không đi thẳng được nữa thì xe quay đầu, dò hướng ngược lại
+ *  Việc dò đường lặp đi lặp lại cho đến khi đến đích
+ *  Các hàm tính khoảng cách, đi sang trái phải hay quay đầu cần thực hiện tinh chỉnh trên board
  */
 
 #include <Servo.h>
+Servo myservo;  // create servo object to control a servo
+//int pos = 0;    // variable to store the servo position
 
-#define PI 3.1415926535897932384626433832795
-
-struct Position  
-{
-  float x, y;
+enum car_status { // Trạng thái của xe
+  UP, LEFT, RIGHT
 };
 
-Servo myservo;  // create servo object to control a servo
-int pos = 0;    // variable to store the servo position
 /******** khai báo chân input/output**************/
-
 const int trig = 6;     // chân trig của SRF-05.
 const int echo = 5;     // chân echo của SRF-05.
-int tien1=10;           // chân IN - A của Module L298.
-int tien2=12;           // chân IN - C của Module L298.
-int lui1=11;            // chân IN - D của Module L298.
-int lui2=13;            // chân IN - B của Module L298.
+int tien1=10;           // chân IN - A của Module L298. banh trai
+int tien2=11;           // chân IN - C của Module L298. banh phai
+int lui1=12;            // chân IN - D của Module L298. banh trai
+int lui2=13;            // chân IN - B của Module L298. banh phai
 int dongcoservo=9;      // chân Orange của Servo.
 
-int gioihan = 15;
+/********** Global variable *******************/
+car_status my_status = UP;
+int gioihanduoi = 25;
+int gioihantren = 100;
 int i;
 int x=0;
 unsigned long thoigian; // biến đo thời gian
 int khoangcach;           // biến lưu khoảng cách
 int khoangcachtrai,khoangcachphai;
-float velocity = (float) 220/3;
-float angular_velocity = (float)52/15;
-float deltaAngle = 45;
-float deltaMove = 1;
-float currentAngle = 90;
-struct Position currentPosition;
-struct Position destinationPos; 
 
-void dokhoangcach();
-void dithang(int duongdi);
-void disangtrai();
-void disangphai();
-void dilui();
+/*********** protoype **************************/
 void resetdongco();
+void resetservo();
+
+void dokhoangcach(); // Cần tinh chỉnh cho phù họp với thực tế trên board
+
+void dithang();
+void disangtrai(); // Ở đây cũng thế
+void disangphai(); // Đây cũng thế
+void dilui();
+void quaydau(); // Đây nữa
+
 void quaycbsangphai();
 void quaycbsangtrai();
+
 void setup() {
-    Serial.begin(9600); //Mở cổng Serial ở 9600
   // put your setup code here, to run once:
-    myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+    myservo.attach(dongcoservo);  // attaches the servo on pin 9 to the servo object
     pinMode(trig,OUTPUT);   // chân trig sẽ phát tín hiệu
     pinMode(echo,INPUT);    // chân echo sẽ nhận tín hiệu
-    
     pinMode(tien1,OUTPUT); 
     pinMode(tien2,OUTPUT); 
     pinMode(lui1,OUTPUT); 
     pinMode(lui2,OUTPUT); 
-    digitalWrite(tien1,LOW);
-    digitalWrite(tien2,LOW);
-    digitalWrite(lui1,LOW);
-    digitalWrite(lui1,LOW);
+
+    resetdongco();
    // Serial.begin(9600);
-    myservo.write(90);    
+    resetservo();
+    khoangcach = 0;    
     delay(500);
-   
-    
-    
 }
 
-void loop() 
-{   
-    findWay();
-    //avoidObstacle();
-    //Serial.println(khoangcach);
-}
-
-void avoidObstacle()
+void loop()
 {
-    khoangcach=0;
-    dokhoangcach();
-
-    Serial.print("Khoảng cách: ");
-    Serial.print(khoangcach);
-    Serial.println("cm");
-    
-    if(khoangcach>gioihan||khoangcach==0) 
+    if (my_status == UP) // thay bang while loop
     {
       dokhoangcach();
-       if(khoangcach>gioihan||khoangcach==0) 
+      if(khoangcach > gioihanduoi || khoangcach == 0) 
+      {
+        dokhoangcach();
+        if(khoangcach > gioihanduoi || khoangcach == 0) 
+        {
+          dithang();   
+        }   
+      }
+      esle
+      {      
+        quaycbsangtrai();  
+        khoangcachtrai=khoangcach;
+        quaycbsangphai();
+        khoangcachphai=khoangcach;
+      
+        if(khoangcachphai < 10 && khoangcachtrai < 10) // thu thay bang gioihanduoi
+        {
+          dilui(); // khong can
+        }
+        else
+        {
+          if(khoangcachphai < khoangcachtrai) 
+          {
+            disangphai();
+            my_status = RIGHT;
+          }
+          else 
+          {
+            disangtrai();
+            my_status = LEFT;
+          }
+          delay(500);
+        }
+      }
+    }
+    if (my_status == LEFT) // while loop ?
+    {
+      dokhoangcach();
+      if(khoangcach > gioihanduoi || khoangcach == 0) 
+      {
+        dokhoangcach(); // khoang cach phia truoc
+        if(khoangcach > gioihanduoi || khoangcach == 0) 
+        {
+          quaycbsangphai();
+          if(khoangcach > gioihanduoi)
+          {
+            dithang(); // di thang them 1 doan
+            delay(1000);
+            disangphai();
+            my_status = UP;
+            delay(500);
+          }  
+        }
+        else 
         {
           dithang();
-          delay(500); 
-        }   
+          delay(500);   
+        }
+      }
+      esle
+      {      
+        quaydau();
+        my_status = RIGHT;
+        delay(500);
+      }
     }
-    else
+    if (my_status == RIGHT)
     {
-      
-      resetdongco();
-      quaycbsangtrai();
-      
-      khoangcachtrai=khoangcach;
-     
-      quaycbsangphai();
-      khoangcachphai=khoangcach;
-      if(khoangcachphai<10&&khoangcachtrai<10){
-        dilui();
-      }
-      else
+      dokhoangcach();
+      if(khoangcach > gioihanduoi || khoangcach == 0) 
       {
-        if(khoangcachphai>khoangcachtrai) 
+        dokhoangcach(); // khoang cach phia truoc
+        if(khoangcach > gioihanduoi || khoangcach == 0) 
         {
-          disangtrai();
-          delay(500);
+          quaycbsangtrai();
+          if(khoangcach > gioihanduoi)
+          {
+            dithang(); // di thang them 1 doan
+            delay(1000);
+            disangtrai();
+            my_status = UP;
+            delay(500);
+          }  
         }
-        if(khoangcachphai<khoangcachtrai) 
+        else 
         {
-          disangphai();
-          delay(500);
+          dithang();
+          delay(500);   
         }
+      }
+      esle
+      {      
+        quaydau();
+        my_status = LEFT;
+        delay(500);
       }
     }
-}
-
-void dithang()
-{ 
-    // Serial.println("go ahead");
-    // resetdongco();
-    // analogWrite(tien1,255);
-    // analogWrite(tien2,255);
-    //delay(2);
-    //unit = 1cm
-    resetdongco(); 
-    analogWrite(tien1, 255);
-    analogWrite(tien2, 255);
-    //delay time for moving 1cm t = s/v
-    float curArgRad = currentAngle * (PI / 180);
-    currentPosition.x -= deltaMove * cos(curArgRad);
-    currentPosition.y -= deltaMove * sin(curArgRad);
-    float time = (deltaMove/ velocity) * 1000;
-    delay(time);
-   
-}
-
-void disangtrai()
-{
-  Serial.println("turn left");
-  resetdongco();
-  digitalWrite(lui1,HIGH);
-  delay(250);
-  digitalWrite(lui1,LOW);
-  
-    
-}
-void disangphai()
-{
-  Serial.println("turn right");
-   resetdongco();
-  digitalWrite(lui2,HIGH);
-  delay(250);
-  digitalWrite(lui2,LOW);
-   
-}
-
-void dilui()
-{
-  Serial.println("step backward");
-  resetdongco();
-  for(i=0;i<20;i++)
-    
-        digitalWrite(lui1,HIGH);
-        digitalWrite(lui2,HIGH);
-        delay(700);
-     
-    digitalWrite(lui1,LOW);
-    digitalWrite(lui2,LOW);
 }
 
 void resetdongco()
 {
-    analogWrite(tien1,0);
-    analogWrite(tien2,0);
-    digitalWrite(lui1,LOW);
-    digitalWrite(lui2,LOW);
+  digitalWrite(tien1,LOW);
+  digitalWrite(tien2,LOW);
+  digitalWrite(lui1,LOW);
+  digitalWrite(lui2,LOW); 
 }
+
+void resetservo()
+{
+   myservo.write(90);
+}
+
 /******** chương trình đo khoảng cách SRF04 ***************/
 void dokhoangcach()
 {
@@ -213,113 +200,73 @@ void dokhoangcach()
     thoigian = pulseIn(echo,HIGH);  
     // Tính khoảng cách đến vật.
     khoangcach = int(thoigian/2/29.412);
-    
+}
 
+/**************** Di chuyen xe ********************/
+void dithang()
+{ 
+        digitalWrite(tien1,HIGH);
+        digitalWrite(tien2,HIGH);
+        //delay(2); 
+}
+
+void disangtrai() // quay trai
+{
+  resetdongco();
+  digitalWrite(lui1,HIGH);
+  delay(250); // chinh delay sao cho xe quay trai 90 do
+  digitalWrite(lui1,LOW);    
+}
+
+void disangphai() // quay phai
+{
+  resetdongco();
+  digitalWrite(lui2,HIGH);
+  delay(250); // chinh delay sao cho xe quay phai 90 do
+  digitalWrite(lui2,LOW);
+}
+
+void dilui()
+{
+  resetdongco();
+  for(i=0;i<20;i++);    
+  digitalWrite(lui1,HIGH);
+  digitalWrite(lui2,HIGH);
+  delay(700); // chinh delay sao cho xe lui hop ly
+  digitalWrite(lui1,LOW);
+  digitalWrite(lui2,LOW);
+}
+
+void quaydau()
+{
+  dilui();
+  if (my_status == LEFT)
+  {
+    disangtrai();
+    delay(500);
+    disangtrai();    
+  }
+  else
+  {
+    disangphai();
+    delay(500);
+    disangphai();
+  }
 }
 
 /*********** chương trình quay cảm biến xang trái *********/
-void quaycbsangtrai()
+void quaycbsangtrai() // do khoan cach ben trai xe
 {
-
     myservo.write(180);              // tell servo to go to position in variable 'pos'
     delay(1000);
     dokhoangcach();
-    myservo.write(90);              // tell servo to go to position in variable 'pos'  
+    resetservo();              // tell servo to go to position in variable 'pos'  
 }
-void quaycbsangphai()
+
+void quaycbsangphai() // do khoang cach ben phai xe
 {
     myservo.write(0);              // tell servo to go to position in variable 'pos'
     delay(1000);
     dokhoangcach();
-    myservo.write(90);              // tell servo to go to position in variable 'pos'
-}
-
-void rotateServo(float angle)
-{
-    myservo.write(angle);              // tell servo to go to position in variable 'pos'
-    Serial.print("Rote servo  angle: ");
-    Serial.println(angle); // tell servo to go to position in variable
-    delay(1000);
-    dokhoangcach();
-    delay(500);
-}
-
-void resetservo()
-{
-    myservo.write(90);              // tell servo to go to position in variable 'pos'
-    delay(1000);
-}
-
-void turn(float angle)
-{
-    resetdongco(); 
-    float angleOnWheel = (angle/360)*4;
-    float roundOnWheel = angle/360;
-    float time = (roundOnWheel / angular_velocity) * 1000;
-    if(angle < 90)
-    {
-        Serial.println("turn right");
-        resetdongco();
-        digitalWrite(tien1,HIGH);
-        delay(time);
-        digitalWrite(tien1,LOW);
-    }
-    else if (angle > 90)
-    {
-        Serial.println("turn right");
-        resetdongco();
-        digitalWrite(tien2,HIGH);
-        delay(time);
-        digitalWrite(tien2,LOW);
-    }
-}
-
-void findWay()
-{
-    resetservo();
-    int angle = 0;
-    float distance = 1000;
-    int arrayDistance[180 / deltaMove]; 
-    int arrayAngle[180 / deltaMove]; 
-    int 
-    while (angle <=  180)
-    {
-        rotateServo(angle);
-        arrayDistance[]
-        if (khoangcach < distance && khoangcach < gioihan)
-        {
-            distance = khoangcach;
-            currentAngle = angle;  
-            break;
-        }
-        angle += deltaAngle;
-    }
-
-    if(distance >= 1000)
-    {
-        turnBackward();
-    }
-    else
-    {
-        turn(angle);
-        dithang();
-    }
-}
-
-void turnBackward()
-{
-    resetdongco(); 
-    analogWrite(lui1, 255);
-    analogWrite(lui2, 255);
-    //delay time for moving 1cm t = s/v
-    float time = (deltaMove / velocity) * 1000;
-    float curArgRad = currentAngle * (PI / 180);
-    currentPosition.x -= deltaMove * cos(curArgRad);
-    currentPosition.y -= deltaMove * sin(curArgRad);
-    delay(time);
-}
-
-void resetservo()
-{
-   myservo.write(90);
+    resetservo();              // tell servo to go to position in variable 'pos'
 }
